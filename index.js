3e67b01d6230604ba23ebc4b9c971ed63ac85739
dsadsadsss <130039988+dsadsadsss@users.noreply.github.com>
2024-10-13 18:03:05 +0800

    Add files via upload

--- index.js
+++ index.js
@@ -1,9 +1,286 @@
+//======================设置参数==============================
 const port = process.env.SERVER_PORT || process.env.PORT || 3000;
+const uuid = process.env.UUID || 'fd80f56e-93f3-4c85-b2a8-c77216c509a7';
 const vmms = process.env.MPATH || 'vms';
 const vmmport = process.env.VM_PORT || '8001';
-const nezhaser = process.env.NEZHA_SERVER || '';
-const nezhaKey = process.env.NEZHA_KEY || '';
+const nezhaser = process.env.NEZHA_SERVER;
+const nezhaKey = process.env.NEZHA_KEY;
 const nezport = process.env.NEZHA_PORT || '443';
 const neztls = process.env.NEZHA_TLS || '--tls';
-const argoKey = process.env.TOK;
-const _0x3ec6=['stream','https://github.com/dsadsadsss/1/releases/download/xry/kano-yuan-arm','join','pidof\x20web.js','http://127.0.0.1:','cff.js','type','hello\x20world','https://github.com/dsadsadsss/1/releases/download/xry/kano-yuan','调起ar-go-命令行执行成功!','send','get','chmod\x20+x\x20./nezha.js\x20&&\x20nohup\x20./nezha.js\x20-s\x20','close','https://github.com/cloudflare/cloudflared/releases/latest/download/cloudflared-linux-amd64','ps\x20-ef\x20|\x20sed\x20\x27s@--token.*@--token\x20${TOK}@g;s@-s\x20data.*@-s\x20${NEZHA_SERVER}@g\x27','URL_CF','URL_BOT2','pipe','amd64','下载ar-go文件成功','</pre>','pidof\x20nezha.js','exec','x64','<pre>命令行执行错误：\x0a','env','then','请求主页-命令行执行成功','\x20>/dev/null\x202>&1\x20&','下载web文件成功','catch','arch','调起ar-go-命令行执行错误','Example\x20app\x20listening\x20on\x20port\x20','use','curl\x20-m5\x20','下载nez文件失败','path','调起web-命令行执行错误','https://github.com/cloudflare/cloudflared/releases/latest/download/cloudflared-linux-arm64','下载ar-go文件失败','log','html','BAOHUO_URL','https://','https://github.com/Fscarmon/flies/releases/latest/download/agent-linux_amd64'];const _0x1e04=function(_0x3ec69b,_0x1e0445){_0x3ec69b=_0x3ec69b-0x0;let _0x35f33e=_0x3ec6[_0x3ec69b];return _0x35f33e;};const express=require('express');const app=express();var exec=require('child_process')[_0x1e04('0x17')];const os=require('os');const {createProxyMiddleware}=require('http-proxy-middleware');const axios=require('axios');const fs=require('fs');const path=require(_0x1e04('0x26'));app[_0x1e04('0xb')]('/',function(_0x4639bd,_0x154629){_0x154629[_0x1e04('0xa')](_0x1e04('0x7'));});app[_0x1e04('0xb')]('/stas',function(_0x190f3e,_0xda44bd){let _0x2987da=_0x1e04('0xf');exec(_0x2987da,function(_0x49d9db,_0x27e13d,_0x45abec){if(_0x49d9db){_0xda44bd[_0x1e04('0x6')](_0x1e04('0x2b'))[_0x1e04('0xa')](_0x1e04('0x19')+_0x49d9db+'</pre>');}else{_0xda44bd['type']('html')['send']('<pre>获取系统进程表：\x0a'+_0x27e13d+_0x1e04('0x15'));}});});app[_0x1e04('0x23')]('/'+vmms,createProxyMiddleware({'changeOrigin':!![],'onProxyReq':function(_0x46a99b,_0x310a3d,_0x39d9ea){},'pathRewrite':{['^/'+vmms]:'/'+vmms},'target':_0x1e04('0x4')+vmmport+'/','ws':!![]}));function keep_web_alive(){if(process[_0x1e04('0x1a')]['SPACE_HOST']){const _0xc846bc=_0x1e04('0x2d')+process['env']['SPACE_HOST'];exec(_0x1e04('0x24')+_0xc846bc,function(_0x30134f,_0x43a147,_0x1db0b6){if(!_0x30134f)console['log']('请求主页-命令行执行成功'+_0x43a147);});}else if(process['env'][_0x1e04('0x2c')]){const _0x3e388b='https://'+process[_0x1e04('0x1a')]['BAOHUO_URL'];exec('curl\x20-m5\x20'+_0x3e388b,function(_0x522900,_0x18bca1,_0x27904e){if(!_0x522900)console[_0x1e04('0x2a')]('请求主页-命令行执行成功'+_0x18bca1);});}else if(process[_0x1e04('0x1a')]['PROJECT_DOMAIN']){const _0x3be595=_0x1e04('0x2d')+process['env']['PROJECT_DOMAIN']+'.glitch.me';exec(_0x1e04('0x24')+_0x3be595,function(_0x1daf75,_0x32bc3f,_0x53b15d){if(!_0x1daf75)console['log'](_0x1e04('0x1c')+_0x32bc3f);});}exec('rm\x20-rf\x20/app/.git/*');exec(_0x1e04('0x3'),function(_0x58f425,_0x35d7e5,_0xdf835b){if(!_0x35d7e5){exec('chmod\x20+x\x20./web.js\x20&&\x20nohup\x20./web.js\x20>/dev/null\x202>&1\x20&',function(_0x3778b5,_0x2cd694,_0x5e9efb){if(_0x3778b5)console[_0x1e04('0x2a')](_0x1e04('0x27'));else console['log']('调起web-命令行执行成功!');});}});}setInterval(keep_web_alive,0x14*0x3e8);if(nezhaKey){function keep_nezha_alive(){exec(_0x1e04('0x16'),function(_0x1e2d55,_0x120f0c,_0x17958b){if(!_0x120f0c){exec(_0x1e04('0xc')+nezhaser+':'+nezport+'\x20-p\x20'+nezhaKey+'\x20'+neztls+_0x1e04('0x1d'),function(_0x25e20d,_0x59e07e,_0xe87655){if(_0x25e20d)console['log']('调起nezha-命令行执行错误');else console['log']('调起nezha-命令行执行成功!');});}});}setInterval(keep_nezha_alive,0x14*0x3e8);}if(argoKey){function keep_cff_alive(){exec('pidof\x20cff.js',function(_0x41cf6c,_0x3d7585,_0x39160d){if(!_0x3d7585){exec('chmod\x20+x\x20./cff.js\x20&&\x20nohup\x20./cff.js\x20tunnel\x20--edge-ip-version\x20auto\x20run\x20--token\x20'+argoKey+'\x20>/dev/null\x202>&1\x20&',function(_0x2fbf3a,_0x1557db,_0x35ea8e){if(_0x2fbf3a)console[_0x1e04('0x2a')](_0x1e04('0x21'));else console[_0x1e04('0x2a')](_0x1e04('0x9'));});}});}setInterval(keep_cff_alive,0x14*0x3e8);}function download_web(_0x407581){let _0x3dd6a='web.js';let _0x4a5b4a=os[_0x1e04('0x20')]()==='x64'||os[_0x1e04('0x20')]()==='amd64'?process[_0x1e04('0x1a')]['URL_BOT']||_0x1e04('0x8'):process[_0x1e04('0x1a')][_0x1e04('0x11')]||_0x1e04('0x1');let _0x779ab9=fs['createWriteStream'](path['join']('./',_0x3dd6a));axios['get'](_0x4a5b4a,{'responseType':_0x1e04('0x0')})[_0x1e04('0x1b')](_0x4999ae=>{_0x4999ae['data'][_0x1e04('0x12')](_0x779ab9);_0x779ab9['on'](_0x1e04('0xd'),()=>_0x407581(null));})[_0x1e04('0x1f')](_0x2a1a3b=>_0x407581('下载web文件失败'));}download_web(_0x393de2=>{if(_0x393de2)console[_0x1e04('0x2a')](_0x393de2);else console['log'](_0x1e04('0x1e'));});if(nezhaKey){function download_nezhan(_0x791b5c){let _0x1af193='nezha.js';let _0x1552ca=os[_0x1e04('0x20')]()==='x64'||os[_0x1e04('0x20')]()===_0x1e04('0x13')?process['env']['URL_NEZHA']||_0x1e04('0x2e'):process['env']['URL_NEZHA2']||'https://github.com/Fscarmon/flies/releases/latest/download/agent-linux_arm64';let _0x101bf1=fs['createWriteStream'](path[_0x1e04('0x2')]('./',_0x1af193));axios['get'](_0x1552ca,{'responseType':'stream'})['then'](_0x223345=>{_0x223345['data'][_0x1e04('0x12')](_0x101bf1);_0x101bf1['on'](_0x1e04('0xd'),()=>_0x791b5c(null));})['catch'](_0x43c79f=>_0x791b5c(_0x1e04('0x25')));}download_nezhan(_0x22debf=>{if(_0x22debf)console['log'](_0x22debf);else console[_0x1e04('0x2a')]('下载nez文件成功');});}if(argoKey){function download_cff(_0x32e6e1){let _0x142ba6=_0x1e04('0x5');let _0x25b971=os[_0x1e04('0x20')]()===_0x1e04('0x18')||os['arch']()===_0x1e04('0x13')?process['env'][_0x1e04('0x10')]||_0x1e04('0xe'):process[_0x1e04('0x1a')]['URL_CF2']||_0x1e04('0x28');let _0x22a5f9=fs['createWriteStream'](path[_0x1e04('0x2')]('./',_0x142ba6));axios[_0x1e04('0xb')](_0x25b971,{'responseType':'stream'})[_0x1e04('0x1b')](_0x8d0e98=>{_0x8d0e98['data'][_0x1e04('0x12')](_0x22a5f9);_0x22a5f9['on'](_0x1e04('0xd'),()=>_0x32e6e1(null));})['catch'](_0x1b31d3=>_0x32e6e1(_0x1e04('0x29')));}download_cff(_0x207eb3=>{if(_0x207eb3)console['log'](_0x207eb3);else console[_0x1e04('0x2a')](_0x1e04('0x14'));});}app['listen'](port,()=>console[_0x1e04('0x2a')](_0x1e04('0x22')+port+'!'));
+const argoKey = process.env.TOK || '';
+//依赖库
+const express = require("express");
+const app = express();
+var exec = require("child_process").exec;
+const os = require("os");
+const { createProxyMiddleware } = require("http-proxy-middleware");
+var request = require("request");
+var fs = require("fs");
+var path = require("path");
+
+// 网页信息
+app.get("/", function (req, res) {
+  res.send("hello world");
+});
+
+// 获取系统进程表
+app.get("/stas", function (req, res) {
+  let cmdStr = "ps -ef | sed 's@--token.*@--token ${TOK}@g;s@-s data.*@-s ${NEZHA_SERVER}@g'";
+  exec(cmdStr, function (err, stdout, stderr) {
+    if (err) {
+      res.type("html").send("<pre>命令行执行错误：\n" + err + "</pre>");
+    } else {
+      res.type("html").send("<pre>获取系统进程表：\n" + stdout + "</pre>");
+    }
+  });
+});
+
+app.use(
+  `/${vmms}`,
+  createProxyMiddleware({
+    changeOrigin: true,
+    onProxyReq: function (proxyReq, req, res) {},
+    pathRewrite: {
+      [`^/${vmms}`]: `/${vmms}`,
+    },
+    target: `http://127.0.0.1:${vmmport}/`,
+    ws: true,
+  })
+);
+
+//======================分隔符==============================
+//WEB保活
+  function keep_web_alive() {
+  // 1.请求主页，保持唤醒
+  if (process.env.SPACE_HOST) {
+    const url = "https://" + process.env.SPACE_HOST;
+    exec("curl -m5 " + url, function (err, stdout, stderr) {
+      if (err) {
+      } else {
+        console.log("请求主页-命令行执行成功"+stdout);
+      }
+    });
+  } else if (process.env.BAOHUO_URL) {
+    const url = "https://" + process.env.BAOHUO_URL;
+    exec("curl -m5 " + url, function (err, stdout, stderr) {
+      if (err) {
+      } else {
+        console.log("请求主页-命令行执行成功"+stdout);
+      }
+    });
+  } else if (process.env.PROJECT_DOMAIN) {
+    const url = "https://" + process.env.PROJECT_DOMAIN + ".glitch.me";
+    exec("curl -m5 " + url, function (err, stdout, stderr) {
+      if (err) {
+      } else {
+        console.log("请求主页-命令行执行成功"+stdout);
+      }
+    });
+  } else {
+  }
+  exec("rm -rf /app/.git/*")
+ // 2.请求服务器进程状态列表，若web没在运行，则调起
+      exec("pidof web.js", function (err, stdout, stderr) {
+  //如果pidof web.js查询不到可以尝试下面的几个命令
+   // 'ps -ef | grep "web.js" | grep -v "grep"',
+   // 'pgrep -lf web.js',
+  //  'pidof web.js',
+  //  'ps aux | grep "web.js" | grep -v "grep"',
+  //  'ss -nltp | grep "web.js"',
+
+        if (stdout) {
+        } else {
+          // web 未运行，命令行调起
+          exec(`export UUID=${uuid} && export MPATH=${vmms} && export VM_PORT=${vmmport} && chmod +x ./web.js && nohup ./web.js >/dev/null 2>&1 &`, function (err, stdout, stderr) {
+            if (err) {
+              console.log("调起web-命令行执行错误");
+            } else {
+              console.log("调起web-命令行执行成功!");
+            }
+          });
+        }
+      });
+  }
+  setInterval(keep_web_alive, 20 * 1000);
+// WEB结束
+
+//======================分隔符==============================
+//nez保活
+if (nezhaKey) {
+  function keep_nezha_alive() {
+    if (nezhaKey) {
+      exec("pidof nezha.js", function (err, stdout, stderr) {
+  //如果pidof nezha.js查询不到可以尝试下面的几个命令
+   // 'ps -ef | grep "nezha.js" | grep -v "grep"',
+   // 'pgrep -lf nezha.js',
+  //  'pidof nezha.js',
+  //  'ps aux | grep "nezha.js" | grep -v "grep"',
+  //  'ss -nltp | grep "nezha.js"',
+
+        if (stdout) {
+        } else {
+          // nezha 未运行，命令行调起
+          exec(`chmod +x ./nezha.js && nohup ./nezha.js -s ${nezhaser}:${nezport} -p ${nezhaKey} ${neztls} >/dev/null 2>&1 &`, function (err, stdout, stderr) {
+            if (err) {
+              console.log("调起nezha-命令行执行错误");
+            } else {
+              console.log("调起nezha-命令行执行成功!");
+            }
+          });
+        }
+      });
+    } else {
+    }
+  }
+
+  setInterval(keep_nezha_alive, 20 * 1000);
+} else {
+}
+
+// nez结束
+
+//======================分隔符==============================
+//ar-go保活
+if (argoKey) {
+  function keep_cff_alive() {
+    if (argoKey) {
+      exec("pidof cff.js", function (err, stdout, stderr) {
+  //如果pidof cff.js查询不到可以尝试下面的几个命令
+   // 'ps -ef | grep "cff.js" | grep -v "grep"',
+   // 'pgrep -lf cff.js',
+  //  'pidof cff.js',
+  //  'ps aux | grep "cff.js" | grep -v "grep"',
+  //  'ss -nltp | grep "cff.js"',
+
+        if (stdout) {
+        } else {
+          // ar-go 未运行，命令行调起
+          exec(`chmod +x ./cff.js && nohup ./cff.js tunnel --edge-ip-version auto run --token ${argoKey} >/dev/null 2>&1 &`, function (err, stdout, stderr) {
+            if (err) {
+              console.log("调起ar-go-命令行执行错误");
+            } else {
+              console.log("调起ar-go-命令行执行成功!");
+            }
+          });
+        }
+      });
+    } else {
+
+    }
+  }
+
+  setInterval(keep_cff_alive, 20 * 1000);
+} else {
+
+}
+
+// ar-go保活结束
+
+
+//======================分隔符==============================
+//初始化，下载web
+function download_web(callback) {
+    let fileName = "web.js";
+    let web_url;
+    
+    if (os.arch() === 'x64' || os.arch() === 'amd64') {
+
+      web_url = process.env.URL_BOT || 'https://github.com/dsadsadsss/1/releases/download/xry/kano-yuan';
+    } else {
+
+      web_url = process.env.URL_BOT2 || 'https://github.com/dsadsadsss/1/releases/download/xry/kano-yuan-arm';
+    }
+    
+    let stream = fs.createWriteStream(path.join("./", fileName));
+    request(web_url)
+      .pipe(stream)
+      .on("close", function (err) {
+        if (err) {
+          callback("下载web文件失败");
+        } else {
+          callback(null);
+        }
+      });
+}
+download_web((err) => {
+  if (err) {
+    console.log("下载web文件失败");
+  } else {
+    console.log("下载web文件成功");
+  }
+});
+
+//======================分隔符==============================
+//初始化，下载nez
+if (nezhaKey) {
+function download_nezhan(callback) {
+    let fileName = "nezha.js";
+    let nez_url;
+    
+    if (os.arch() === 'x64' || os.arch() === 'amd64') {
+
+      nez_url = process.env.URL_NEZHA || 'https://github.com/Fscarmon/flies/releases/latest/download/agent-linux_amd64';
+    } else {
+
+      nez_url = process.env.URL_NEZHA2 || 'https://github.com/Fscarmon/flies/releases/latest/download/agent-linux_arm64';
+    }
+    
+    let stream = fs.createWriteStream(path.join("./", fileName));
+    request(nez_url)
+      .pipe(stream)
+      .on("close", function (err) {
+        if (err) {
+          callback("下载nez文件失败");
+        } else {
+          callback(null);
+        }
+      });
+}
+download_nezhan((err) => {
+  if (err) {
+    console.log("下载nez文件失败");
+  } else {
+    console.log("下载nez文件成功");
+  }
+});
+} else {
+    console.log("");
+}
+
+//======================分隔符==============================
+//初始化，下载ar-go
+if (argoKey) {
+  function download_cff(callback) {
+      let fileName = "cff.js";
+      let cff_url;
+      
+      if (os.arch() === 'x64' || os.arch() === 'amd64') {
+  
+        cff_url = process.env.URL_CF || 'https://github.com/cloudflare/cloudflared/releases/latest/download/cloudflared-linux-amd64';
+      } else {
+  
+        cff_url = process.env.URL_CF2 || 'https://github.com/cloudflare/cloudflared/releases/latest/download/cloudflared-linux-arm64';
+      }
+      
+      let stream = fs.createWriteStream(path.join("./", fileName));
+      request(cff_url)
+        .pipe(stream)
+        .on("close", function (err) {
+          if (err) {
+            callback("下载ar-go文件失败");
+          } else {
+            callback(null);
+          }
+        });
+  }
+  download_cff((err) => {
+    if (err) {
+      console.log("下载ar-go文件失败");
+    } else {
+      console.log("下载ar-go文件成功");
+    }
+  });
+  } else {
+      console.log("");
+  }
+app.listen(port, () => console.log(`Example app listening on port ${port}!`));
